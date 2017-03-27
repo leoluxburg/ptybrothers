836f50cae0b9fd1a9bf4492d1120481327ee46c6
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-03-27 14:30:36 +0200

    PTY Brothers V.0.0.1

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/photos'
+
+  get 'pages/videos'
+
+  get 'pages/events'
+
+  get 'pages/riders'
+
+  get 'pages/about'
+
+  get 'pages/dutary'
+
+  get 'pages/album'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
